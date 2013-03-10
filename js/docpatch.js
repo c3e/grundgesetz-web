d643c5cab22809949dcff9930220880c2c413e10
Benjamin Heisig <benjamin@heisig.name>
2013-03-10 13:53:03 +0100

    Improved some texts and fixed download

--- js/docpatch.js
+++ js/docpatch.js
@@ -1,9 +1,3 @@
-var timeline_config = {
-    width: "100%",
-    height: "100%",
-    source: 'grundgesetz_meta.json'
-}
-
 ks.ready(function() {
     $('.nav').onePageNav({
         currentClass: 'active',
@@ -14,48 +8,50 @@ ks.ready(function() {
     $.ajax({
         url: "grundgesetz-dev/etc/meta.json",
         dataType: "json"
-    }).done(function(meta) {
+    }).done(function(data) {
+        var meta = data.reverse();
+
         $.each(meta, function() {
             $('#revision').append('<option value="' + this.id + '_' + this.announced + '">' + this.id + '. ' + this.title + ' (' + this.announced + ')</option>');
         });
-    });
     
-    var formats = [
-        {
-            "title": "PDF",
-            "ext": "pdf"
-        },
-        {
-            "title": "eBook (EPUB)",
-            "ext": "epub"
-        },
-        {
-            "title": "HTML",
-            "ext": "html"
-        },
-        {
-            "title": "Open Document Format (ODT)",
-            "ext": "odt"
-        },
-        {
-            "title": "Plain text",
-            "ext": "txt"
-        }
-    ];
-    
-    $.each(formats, function() {
-        $('#format').append('<option value="' + this.ext + '">' + this.title + '</option>');
-    });
-    
-    $('#download').attr(
-        'action',
-        'grundgesetz-dev/out/brd_grundgesetz_' + $('#revision').val() + '.' + $('#format').val()
-    );
-    
-    $('#revision, #format').change(function() {
+        var formats = [
+            {
+                "title": "PDF",
+                "ext": "pdf"
+            },
+            {
+                "title": "eBook (EPUB)",
+                "ext": "epub"
+            },
+            {
+                "title": "HTML",
+                "ext": "html"
+            },
+            {
+                "title": "Open Document Format (ODT)",
+                "ext": "odt"
+            },
+            {
+                "title": "Plain text",
+                "ext": "txt"
+            }
+        ];
+        
+        $.each(formats, function() {
+            $('#format').append('<option value="' + this.ext + '">' + this.title + '</option>');
+        });
+        
         $('#download').attr(
             'action',
             'grundgesetz-dev/out/brd_grundgesetz_' + $('#revision').val() + '.' + $('#format').val()
         );
+        
+        $('#revision, #format').change(function() {
+            $('#download').attr(
+                'action',
+                'grundgesetz-dev/out/brd_grundgesetz_' + $('#revision').val() + '.' + $('#format').val()
+            );
+        });
     });
 });

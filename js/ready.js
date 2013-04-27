280cfef0655b593a3be035cf009d32d8aa2e095f
Benjamin Heisig <benjamin@heisig.name>
2013-04-27 16:05:39 +0200

    Moved JS to external file

---
+++ js/ready.js
@@ -0,0 +1,20 @@
+ks.ready(function () {
+    /**
+     * Configures OnePageNav.
+     */
+    $('#topnav').onePageNav({
+        currentClass: 'current',
+        changeHash: false,
+        scrollSpeed: 750,
+        scrollOffset: 80
+    });
+
+    /**
+     * Configures and initiates DocPatch.
+     */
+    var docpatch = new DocPatch({
+        prefix: "brd_grundgesetz_",
+        repoDir: "grundgesetz-dev",
+        dateFormat: "dd.mm.yy"
+    });
+});
